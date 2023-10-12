5c9e0d33aa73317c62f69a70e3c2a40f0c222374
Matt Beruldsen <mberulds@gmail.com>
2023-10-12 21:01:38 +1100

    created all controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,32 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 require 'faker'
+require 'open-uri'
 
 puts 'Cleaning database...'
 Movie.destroy_all
 
-puts 'Creating movies...'
-10.times do
-  attributes = {
-    title: Faker::Movie.title,
-    overview: Faker::Movie.quote,
-    rating: Faker::Number.between(from: 1, to: 10)
-  }
-  movie = Movie.create!(attributes)
-  puts "Created #{movie.title}"
+url = 'http://tmdb.lewagon.com/movie/top_rated?'
+
+response = URI.open(url)
+data = JSON.parse(response.read)
+random_movies = data['results'].sample(10)
+random_movies.each do |movie_data|
+  title = movie_data['title']
+  overview = movie_data['overview']
+  poster_url = "https://image.tmdb.org/t/p/w200#{movie_data['poster_path']}"
+  rating = movie_data['vote_average'].round
+  Movie.create({ title: title, overview: overview, poster_url: poster_url, rating: rating })
 end
+
+# puts 'Creating movies...'
+# 10.times do
+#   attributes = {
+#     title: Faker::Movie.title,
+#     overview: Faker::Movie.quote,
+#     rating: Faker::Number.between(from: 1, to: 10)
+#   }
+#   movie = Movie.create!(attributes)
+#   puts "Created #{movie.title}"
+# end
 puts 'Completed creations'
